989782db59db7d99f1d2221e93a1405344eb8b8a
Ste <stephenkelehan@gmail.com>
2017-10-30 14:39:49 +0000

    Sending a file Via Server Client

--- client.py
+++ client.py
@@ -4,15 +4,25 @@ import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
 
-port = 9999
+port = 1234
 
 #Connect to hostname
 s.connect((host,port))
+s.send("Hello server!")
 
-# Resve limit no. of bytes! -> this instance 1024
-mess = s.recv(1024)
+with open('file_received','wb') as f:
+    print('file opened')
+    while 1:
+        print('receiving data ..')
+        data = s.recv(1024)
+        print('data ' + data)
+        if not data:
+            break
+        #write data to a file
+        f.write(data)
+
+f.close()
+print('Successfully got the file')
 s.close()
-
-print("The time got from server is " + mess.decode('ascii'))
-
+print('connection closed')
 
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Setting localhost! -> use function to fine local machine name and set port!
 host = socket.gethostname()
 
-port = 9999
+port = 1234
 
 # Get link to the port
 serversocket.bind((host, port))
@@ -16,13 +16,30 @@ serversocket.bind((host, port))
 # Execpt 5 requests -> use listen, listens for connects to the socket
 serversocket.listen(5)
 
+print ('Server wating for connection..... ')
+
 while 1:
     #connection, get and print address 
     clientsocket,addr = serversocket.accept()
     #accept() -> returns two values, one new socket obj and the address of the connection
     print("Have connection with " + str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
+    data = clientsocket.recv(1024)
+    print('Server resived' + repr(data))
+    
+    #open a file and read 1024 bytes at a time
+    filename = 'Testfile.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while(l) :
+        #Sending line read from texty file l -> 1024 
+        clientsocket.send(l)
+        print('Sent' , repr(l))
+        print('\n')
+        l = f.read(1024)
+    f.close()
+
+    print('Finished Sending')
+    clientsocket.send('Thanks')
     clientsocket.close()
 
 
