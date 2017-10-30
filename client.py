060ab3247677fcabc06baca0f0b3cbad189ee0e9
Ste <stephenkelehan@gmail.com>
2017-10-30 19:26:38 +0000

    Chat server with multiple clients

--- client.py
+++ client.py
@@ -1,30 +1,61 @@
 import socket
-import time
+from threading import Thread
+from SocketServer import ThreadingMixIn
+import sys
+import select
 
-# Creating a socket object! 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def client():
+    # if the length of the command line arg is less than 3 -> exit
+    if (len(sys.argv) < 3):
+        print (" Usage : puthon chat_client.py hostname port")
+        sys.exit()
 
-# Setting localhost! -> use function to fine local machine name and set port!
-host = 'localhost'
-port = 1234
-BufferSize = 1024
-
-# Get link to the port
-serversocket.connect((host, port))
-
-with open('Received_file.txt', 'wb') as f:
-    print ('file opened')
-    while (1):
-        data = serversocket.recv(BufferSize)
-        print('data ' + data )
-        if not data:
-            f.close()
-            print( 'file closed' )
-            break
-        f.write(data)
-
-print('Successfully get the file')
-serversocket.close()
-print('Connection Closed')
+    #the host name is the first command line argument
+    host = sys.argv[1]
+    #the port is the second command line aregument
+    port = sys.argv[2]
+    
+    print( "Host: " + host + " Port: " + port)
 
 
+    # Set up socket and set it to time out
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.settimeout(2)
+
+    #connect to client/host
+    try:
+        s.connect((host,port))
+    except:
+        print("Unable to connect to host")
+        sys.exit()
+
+    print("Connect to remot host, You can start to chat! Woooo")
+    # Print to comandline forces the buffer to be flushed and writes everything in the buffer even if it woulf wait nomally
+    sys.stdout.write('[Me] '); sys.stdout.flush()
+    
+    while True:
+        socket_list = [sys.stdin, s]
+        
+        # Get list of sockets which are readable
+        ready_to_read,ready_to_write,in_error = select.select(socket_list, [] , [])
+
+        for sock in ready_to_read:
+            if sock == s:
+                # incoming message from remote server, s
+                data = sock.recv(4096)
+                if not data:
+                    print("\nDisconnected form chat server")
+                    sys.exit()
+                else:
+                    # Print the data
+                    sys.stdout.write(data)
+                    sys.stdout.write("[Me] "); sys.stdout.flush()
+            else:
+                # get message user have entered
+                message = sys.stdin.readline()
+                s.send(message)
+                sys.stdout.write("[Me] "); sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    sys.exit(client())
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import socket
 from threading import Thread
 from SocketServer import ThreadingMixIn
+import sys
+import select
+
 
 #Creatring a TCP Server
 #create a socket obj
 host = 'localhost'
-port = 1234
-BufferSize = 1024
+port = 2345
+BufferSize = 4096
+socket_list = []
 
 #################################
 #                               #
@@ -14,35 +18,113 @@ BufferSize = 1024
 #                               #
 #################################
 
-class ClientThread(Thread):
+#class ClientThread(Thread):
+#
+#    def __init__(self, ip, port, sock):
+#        Thread.__init__(self)
+#        self.ip = ip
+#        self.port = port
+#        self.sock = sock
+#        print("New thread IP: " + ip + "Port " + str(port))
+#
+#    def run(self):
+#        filename = 'Testfile.txt'
+#        f = open(filename, 'rb')
+#        while (1):
+#            l = f.read(BufferSize)
+#            while(l):
+#                self.sock.send(l)
+#                l = f.read(BufferSize)
+#            if not l:
+#                f.close()
+#                self.sock.close()
+#                break
 
-    def __init__(self, ip, port, sock):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.sock = sock
-        print("New thread IP: " + ip + "Port " + str(port))
 
-    def run(self):
-        filename = 'Testfile.txt'
-        f = open(filename, 'rb')
-        while (1):
-            l = f.read(BufferSize)
-            while(l):
-                self.sock.send(l)
-                l = f.read(BufferSize)
-            if not l:
-                f.close()
-                self.sock.close()
-                break
+
+def server():
+    
+    #setting up sockets
+    socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # socket.setsockopt(level,optname, value) 
+    socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_tcp.bind((host, port))
+    socket_tcp.listen(10)
+
+    socket_list.append(socket_tcp)       # add server socket to tyhe list of readable connections
+    print("Chat server started on Port: " + str(port) + " HostName: " + str(host))
+    while True:
+        # get the list of sockets
+        # 4th argument, timeout = 0 :poll and never block
+        ready_to_read, ready_to_write, in_error = select.select(socket_list, [],[], 0)
+
+        for sock in ready_to_read:
+            # a new connection request recived
+            if sock == socket_tcp:
+                sockfd, addr = server_scoket.accept()
+                SPCLET_LIST.append(sockfd)
+                print("Connected")
+
+                broadcast(socket_tcp, sockfd, addr + " : " + addr + " entered the chat room\n")
+            else:
+                try:
+                     # process data taken form client
+                     data = sock.recv(RECV_BUFFER)
+                     if data:
+                         #there is somthing in socket
+                         broadcast(socket_tcp, sock, "\r" + "[" + str(sock.getpeername()) + "] " + data)
+                     else:
+                         if sock in socket_list:
+                             socket_list.remove(sock)
+
+                         # this means that the connect is prob broken! thus close connection
+                         broadcase(socket_tcp,sock, "Client " + addr + " : " + addr + " is offline")
+
+                except:
+                    broadcast(socket_tcp,"Client " + addr + " : " + addr + " is offline")
+                    continue
+            socket_tcp.close()
+
+
+
+def broadcast (server_socket, sock, message):
+    for socket in SOCKET_LIST:
+        # send message to spespic person
+        if socket != server_socket and sock != sock :
+            try:
+                socket.send(message)
+            except:
+                #broken socket connection
+                socket.close()
+                if socket in SOCKET_LIST:
+                    SOCKET_LIST.remove(socket)
+
+if __name__ == "__main__":
+    sys.exit(server())
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #setting up sockets
-socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # socket.setsockopt(level,optname, value) 
-socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_tcp.bind((host, port))
+#socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#socket_tcp.bind((host, port))
 #creating an empty array of threads
-threads= []
+#threads= []
 
 ###############################################
 #                                             #
@@ -50,23 +132,23 @@ threads= []
 #                                             #
 ###############################################
 
-while 1:
-    # Listen for connect to port 
-    socket_tcp.listen(5)
-    print("Awaiting connection \n")
-    #Returning two values new sock obj and address of connection 
-    (conn, (ip,port)) = socket_tcp.accept()
-    print("Connected to IP: " + str(ip) + 'Port: ' + str(port))
-    # Creadting new thread for multiple connections
-    newthread = ClientThread(ip,port,conn)
-    # starting a new thread
-    newthread.start()
-    # add to the list 
-    threads.append(newthread)
+#while 1:
+#    # Listen for connect to port 
+#    socket_tcp.listen(5)
+#    print("Awaiting connection \n")
+#    #Returning two values new sock obj and address of connection 
+#    (conn, (ip,port)) = socket_tcp.accept()
+#    print("Connected to IP: " + str(ip) + 'Port: ' + str(port))
+#    # Creadting new thread for multiple connections
+#    newthread = ClientThread(ip,port,conn)
+#    # starting a new thread
+#    newthread.start()
+#    # add to the list 
+#    threads.append(newthread)
 
 
-for t in threads:
-    t.join()
+#for t in threads:
+#    t.join()
 
 
 
